b71277d3bb44c41186d61643c0357034c0620d03
kallePohjanjoki <kalle@pohjanjoki.com>
2025-03-29 16:37:50 +0200

    Finish mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,62 +3,50 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
 
-const finalWord = "Go!"
-const countdownStart = 3
-
+// Sleeper allows you to put delays.
 type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
+// ConfigurableSleeper is an implementation of Sleeper with a defined delay.
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+// Sleep will pause execution for the defined Duration.
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
-type SpyCountdownOperations struct {
-	Calls []string
-}
+const finalWord = "Go!"
 
-func (s *SpyCountdownOperations) Sleep() {
-	s.Calls = append(s.Calls, sleep)
-}
-
-func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
-	s.Calls = append(s.Calls, write)
-	return
-}
-
-const write = "write"
-const sleep = "sleep"
-
-
-
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper.
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
-			sleeper.Sleep()
-		}
+	for i := range countDownFrom(3) {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
 
-		for i := countdownStart; i > 0; i-- {
-			fmt.Fprintln(out, i)
-		}
+	fmt.Fprint(out, finalWord)
+}
 
-		fmt.Fprint(out, finalWord)
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
